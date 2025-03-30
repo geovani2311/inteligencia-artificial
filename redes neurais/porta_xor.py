3b8339ba4388008ca16fc0586f8f170b7b6cc65f
geovani2311 <geovani.cardoso1@outlook.com>
2025-03-30 19:57:44 -0300

    pesos

--- redes neurais/porta_xor.py
+++ redes neurais/porta_xor.py
@@ -1,9 +1,13 @@
 import math 
+import random
 
 #dados de entrada
 x1 = [0,0,1,1]
 x1 = [0,1,0,1]
 classe = [0,1,0,1]
 
+#pesos gerados aleatoriamente
+pesos = random
+
 funcao_soma = (xi * wi) + (x2 + w2)
-funcao_ativacao = 1 / 1 + e - x 
+funcao_ativacao = 1 / (1 + e - x)
