420bfb1194d1b6eac56cf89397e4d43028dcbc45
geovani2311 <geovani.cardoso1@outlook.com>
2025-04-02 00:07:36 -0300

    funcao sigmoide e ativacao

--- redes neurais/porta_xor.py
+++ redes neurais/porta_xor.py
@@ -1,13 +1,26 @@
+import pandas as pd
+import numpy as np
 import math 
 import random
 
 #dados de entrada
-x1 = [0,0,1,1]
-x1 = [0,1,0,1]
-classe = [0,1,0,1]
+#x1 = [0,0,1,1]
+#x1 = [0,1,0,1]
+#classe = [0,1,0,1]
 
 #pesos gerados aleatoriamente
-pesos = random
+peso1 = random.random()
+peso2 = random.random()
+print(peso1,peso2)
 
-funcao_soma = (xi * wi) + (x2 + w2)
-funcao_ativacao = 1 / (1 + e - x)
+def funcao_soma():
+    soma = (x1 * peso1) + (x2 * peso2)
+    return soma
+
+def funcao_sigmoide():
+    sigmoide = 1 / (1 + math.exp(-x))
+    return sigmoide
+
+#def funcao_ativacao():
+#    calculo = (x1 * peso1) + (x2 * peso2)
+#    return calculo
